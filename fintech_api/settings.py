113e7c5afbd287abe54ab6deb349f6af872ee4e1
Sophia Iroegbu <iroegbusophia3@gmail.com>
2023-10-06 12:43:10 +0100

    Update settings.py

--- fintech_api/settings.py
+++ fintech_api/settings.py
@@ -154,7 +154,7 @@ LOGIN_REDIRECT_URL = '/docs'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
