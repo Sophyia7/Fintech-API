81cfa146a7c199515d86e12ec393788db0b7abdb
Sophyia7 <iroegbusophia3@gmail.com>
2023-10-06 01:39:18 +0100

    Modified

--- fintech_api/deployment.py
+++ fintech_api/deployment.py
@@ -21,7 +21,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # connection_string = os.environ['ZURE_CONNECTIONSTRING']
 # parameters = {pair.split(':')[0]: pair.split(':')[1] for pair in connection_string.split(';')}
