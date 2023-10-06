93134b7526899eb06416bb70fe9c372bb5d378fe
Sophyia7 <iroegbusophia3@gmail.com>
2023-10-06 01:03:58 +0100

    Modifed for deployment

--- fintech_api/settings.py
+++ fintech_api/settings.py
@@ -152,6 +152,7 @@ LOGIN_REDIRECT_URL = '/docs'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
