37ad1663ca7b0ec009aad95192cb78a39e648641
Sophia Iroegbu <iroegbusophia3@gmail.com>
2023-10-06 12:02:14 +0100

    Update settings.py

--- fintech_api/settings.py
+++ fintech_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fintech-app.azurewebsites.net"]
 
 # Application definition
 
